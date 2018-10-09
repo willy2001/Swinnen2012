09b4ad77b437f0a976d6f3f07b26dfd7ea01f26c
willy.2001 <2001.willychauvin@gmail.com>
2018-10-09 19:17:10 +0200

    chapitre 4

--- Moi/chap4.py
+++ Moi/chap4.py
@@ -1,2 +1,25 @@
 # -*- coding: utf-8 -*-
+"""
+Spyder Editor
+"""
+#exercice 4,1
+a, b, c, d = 3, 4, 5, 7
+a, c = c, a
 
+print(a, b, c, d)
+
+#exercice 4,2
+a = 0
+while a < 20:
+    a = a +1
+    print(a*7)
+    
+    #exercice 4,3
+b = 1
+while b<= 16384:
+    print(b, b*1.65)
+    b = b*2
+    
+    #exercice 4,5
+l, h, p = 5, 6, 7
+print(l*h*p)
